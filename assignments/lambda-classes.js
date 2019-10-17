8d6b1f45632b8dc8ef3cb95603ef2ef45b4f4c0e
Apiyo4 <odingoannette@gmail.com>
2019-10-17 15:28:50 +0300

    Create a class Instructor that extends from the Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -9,3 +9,25 @@ class Person{
         return `Hello my name is ${this.name}, I am from ${this.location}`;
     }
 }
+class Instructor extends Person{
+    constructor(attributes){
+    super(attributes);
+    this.specialty = attributes.specialty;
+    this.favlanguage = attributes.favlanguage;
+    this.catchPhrase = attributes.catchPhrase;
+   }
+   demo(subject){
+    return `Today we are learning about ${subject}`;
+   }
+   grade(student, subject){
+   return `${student} receives a perfect score on ${subject}`;
+   }
+}
+const Ada = new Instructor({
+    name: "Ada",
+    age: 23,
+    location: "Kenya",
+    specialty: "redux",
+    favlanguage: "Python",
+    catchPhrase:"Welcome to Python"
+});
